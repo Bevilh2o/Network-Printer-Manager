a981e7d2bce874a10d78c558ac531974399e00ff
Bevi <michelbevilacqua@yahoo.it>
2019-08-30 00:57:58 +0200

    Index updated

--- NetworkPrinterManager.ps1
+++ NetworkPrinterManager.ps1
@@ -5,8 +5,8 @@ $isDone = $false;
 while (!$isDone)
 {
 	echo 'Enter the number corresponding to the printer you want to install and press "Enter"'
-	echo '0. INSERT PRINTER NAME HERE'
 	echo '1. INSERT PRINTER NAME HERE'
+	echo '2. INSERT PRINTER NAME HERE'
 	$Printer = Read-Host -Prompt 'Number'
 	<# 
 	Exception handling still need to be implemented
@@ -16,8 +16,8 @@ while (!$isDone)
 		switch ($Printer)
 		{
 			#EXAMPLE: $result = add-printerport -name "10.40.100.20" -printerhostaddress "10.40.100.20"; add-printer -name "LF-LEXMARK" -drivername "Lexmark XC9200 Series PS3" -port "10.40.100.20"; break
-			0 { $result = add-printerport -name "INSERT PRINTER PORT HERE" -printerhostaddress "INSERT PRINTER IP HERE"; add-printer -name "INSERT PRINTER NAME HERE" -drivername "INSERT PRINTER DRIVER NAME HERE" -port "INSERT PORT HERE"; break}   
-			1 { $result = add-printerport -name "INSERT PRINTER PORT HERE" -printerhostaddress "INSERT PRINTER IP HERE"; add-printer -name "INSERT PRINTER NAME HERE" -drivername "INSERT PRINTER DRIVER NAME HERE" -port "INSERT PORT HERE"; break}
+			1 { $result = add-printerport -name "INSERT PRINTER PORT HERE" -printerhostaddress "INSERT PRINTER IP HERE"; add-printer -name "INSERT PRINTER NAME HERE" -drivername "INSERT PRINTER DRIVER NAME HERE" -port "INSERT PORT HERE"; break}   
+			2 { $result = add-printerport -name "INSERT PRINTER PORT HERE" -printerhostaddress "INSERT PRINTER IP HERE"; add-printer -name "INSERT PRINTER NAME HERE" -drivername "INSERT PRINTER DRIVER NAME HERE" -port "INSERT PORT HERE"; break}
 			default {"Invalid number"; break}
 		}
 
@@ -35,16 +35,16 @@ while (!$isDone)
 	}
 	#>
 	
-	echo 'Would you like to install another printer? Enter the corresponding number and press "Enter"'
-	echo '0. Yes'
-	echo '1. No'
+	echo 'Would you like to install another printer? Enter the corresponding letter and press "Enter"'
+	echo 'Enter "y" for "Yes"'
+	echo 'Enter "n" for "No"'
 	
-	$Continue = Read-Host -Prompt 'Number'
+	$Continue = Read-Host -Prompt 'Letter'
 	
 	switch ($Continue)
 	{
-		0 {break}   
-		1 {$isDone = $true; break}
+		y {break}   
+		n {$isDone = $true; break}
 		default {$isDone = $true; break}
 	}
 }
